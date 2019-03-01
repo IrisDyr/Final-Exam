e58ba0a2812f3903b8232dc29dfe7fdac83ae60a
Iris <Irisdyrmishi@gmail.com>
2019-03-01 15:31:41 +0200

    Practical Exercise

--- models/user_model.py
+++ models/user_model.py
@@ -12,5 +12,4 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    
